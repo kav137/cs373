2da93fb740a4e63337c5a13eb464dc841f2f084c
Artyom <artyom.koval.137@gmail.com>
2017-08-03 01:56:01 +0300

    Searching
    Fixed cost of path.
    Added call of search.

--- Search/SimpleNavigation.py
+++ Search/SimpleNavigation.py
@@ -14,12 +14,12 @@
 # Grid format:
 #   0 = Navigable space
 #   1 = Occupied space
-
 grid = [[0, 0, 1, 0, 0, 0],
         [0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 1, 0],
         [0, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0]]
+
 init = [0, 0]
 goal = [len(grid) - 1, len(grid[0]) - 1]
 cost = 1
@@ -35,22 +35,33 @@ delta_name = ['^', '<', 'v', '>']
 def search(grid, init, goal, cost):
     expand = [[0, init[0], init[1]]]
     expand_next = []
+    path = False
 
-    for i in range(11):
+    while path is False:
         for cell_to_expand in expand:
             cell_path, cell_y, cell_x = cell_to_expand
+            grid[cell_y][cell_x] = -1
             for movement in delta:
                 movement_y, movement_x = movement
                 target_cell_y, target_cell_x = [cell_y + movement_y, cell_x + movement_x]
                 if (len(grid) > target_cell_y >= 0 and len(grid[0]) > target_cell_x >= 0
                     and grid[target_cell_y][target_cell_x] == 0):
-                    expand_next.append([cell_path + 1, target_cell_y, target_cell_x])
+                    expand_next.append([cell_path + cost, target_cell_y, target_cell_x])
                     grid[target_cell_y][target_cell_x] = -1
 
-        print expand_next
+        if len(expand_next) == 0:
+            path = 'fail'
+            # exit(0) this line is not accepted when using cs373's course validation
+
+        for result in expand_next:
+            length, x, y = result
+            if x == goal[0] and y == goal[1]:
+                path = result
+
         expand = expand_next
         expand_next = []
-        # return path
+
+    return path
 
 
-search(grid, init, goal, cost)
+print search(grid, init, goal, cost)
